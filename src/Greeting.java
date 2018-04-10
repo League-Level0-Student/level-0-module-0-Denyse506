c9b7f8ddf1d512ee13d6d45bafe02704488c6988
38558 <38558@0338S-Z-90GSYPY.sdcs.local>
2018-04-10 14:25:30 -0700

    input and output

---
+++ src/Greeting.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class Greeting {public static void main(String[] args) {
+JOptionPane.showMessageDialog(null, "What is your name");
+	System.out.print("What is your name");
+	String Username=JOptionPane.showInputDialog("Do you know how to code");
+if(Username.equals("Yes")) {
+	JOptionPane.showMessageDialog(null,"you will ruler the world ");
+}
+	else { 
+		JOptionPane.showMessageDialog(null, "You suck");
+		
+	}
+	
+	
+}
+
+	
+}
---
+++ src/IntroToInputOutput.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class IntroToInputOutput {
+public static void main(String[] args) {
+JOptionPane.showMessageDialog(null, "Tacos");
+System.out.print("thier");
+String numTacos=JOptionPane.showInputDialog("hi");
+System.out.println(numTacos);
+
+
+}
+}
