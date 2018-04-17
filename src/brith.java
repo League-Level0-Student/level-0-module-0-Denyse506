c6788b5784e4aca8aee9e5dac80d752a32e02b58
38558 <38558@0338S-Z-90GSYPY.sdcs.local>
2018-04-17 14:20:55 -0700

    add number together

---
+++ src/brith.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class brith {
+	public static void main(String[] args) {
+		String number1=JOptionPane.showInputDialog(null,"Enter your first number");
+		String number2 =JOptionPane.showInputDialog(null,"enter you second number ");
+	int size2=Integer.parseInt(number1);
+	int size3=Integer.parseInt(number2);
+	JOptionPane.showMessageDialog(null, size2 +size3);
+	}
+
+	
+	}
+	
+
